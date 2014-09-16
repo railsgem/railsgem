e32405554bb2719ae79edd0f3de47e81d48f5876
railsgem <jun.chenying@hotmail.com>
2014-09-16 16:01:38 +0800

    Initial Commit

---
+++ iOS/私人通讯录/私人通讯录/CYViewController.h
@@ -0,0 +1,13 @@
+//
+//  CYViewController.h
+//  私人通讯录
+//
+//  Created by Rails Chen on 14-9-16.
+//  Copyright (c) 2014年 Railsgem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYViewController : UIViewController
+
+@end
---
+++ iOS/私人通讯录/私人通讯录/CYViewController.m
@@ -0,0 +1,29 @@
+//
+//  CYViewController.m
+//  私人通讯录
+//
+//  Created by Rails Chen on 14-9-16.
+//  Copyright (c) 2014年 Railsgem. All rights reserved.
+//
+
+#import "CYViewController.h"
+
+@interface CYViewController ()
+
+@end
+
+@implementation CYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
