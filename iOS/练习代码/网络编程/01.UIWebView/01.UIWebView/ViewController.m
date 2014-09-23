260dfa913fd017e6ad3994c7d3cba53feb7563b5
railsgem <jun.chenying@hotmail.com>
2014-09-23 17:06:59 +0800

    03.UIWebView实现浏览器
    03.UIWebView实现浏览器

--- iOS/练习代码/网络编程/01.UIWebView/01.UIWebView/ViewController.m
+++ iOS/练习代码/网络编程/01.UIWebView/01.UIWebView/ViewController.m
@@ -8,9 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UISearchBarDelegate>
+@interface ViewController ()<UISearchBarDelegate, UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
 
 @end
 
@@ -20,18 +22,16 @@
 {
     [super viewDidLoad];
     
-    //1.定义URL
-    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
-    
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    
-    [self.webView loadRequest:request];
+    [self loadString:@"http://m.baidu.com/"];
 }
 
 //让浏览器加载指定的字符串
 -(void)loadString:(NSString *)str
 {
-    NSString *urlStr = [NSString stringWithFormat:@"http://m.baidu.com/s?word=%@", str];
+    NSString *urlStr = str;
+    if (![str hasPrefix:@"http://"]) {
+        urlStr = [NSString stringWithFormat:@"http://m.baidu.com/s?word=%@", str];
+    }
     
     //1.定义URL
     NSURL *url = [NSURL URLWithString:urlStr];
@@ -47,11 +47,22 @@
 {
     NSLog(@"searchBarSearchButtonClicked - %@", searchBar.text);
     [self loadString:searchBar.text];
+    
+    [self.view endEditing:YES];
 }
 
 -(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
 {
     NSLog(@"searchText - %@", searchText);
 }
+
+
+#pragma mark -webView代理方法
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    self.backButton.enabled = webView.canGoBack;
+    self.forwardButton.enabled = webView.canGoForward;
+}
+
 
 @end
