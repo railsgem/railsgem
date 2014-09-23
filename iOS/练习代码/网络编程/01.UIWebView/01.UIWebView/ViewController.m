29334a3b958e4575e1525df322cdbd3f04487916
railsgem <jun.chenying@hotmail.com>
2014-09-23 16:21:27 +0800

    02.UIWebView简单使用
    02.UIWebView简单使用

--- iOS/练习代码/网络编程/01.UIWebView/01.UIWebView/ViewController.m
+++ iOS/练习代码/网络编程/01.UIWebView/01.UIWebView/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -17,13 +19,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //1.定义URL
+    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [self.webView loadRequest:request];
 }
 
-- (void)didReceiveMemoryWarning
+//让浏览器加载指定的字符串
+-(void)loadString:(NSString *)str
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSString *urlStr = [NSString stringWithFormat:@"http://m.baidu.com/s?word=%@", str];
+    
+    //1.定义URL
+    NSURL *url = [NSURL URLWithString:urlStr];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [self.webView loadRequest:request];
+    
+}
+
+#pragma mark -搜索栏代理方法
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    NSLog(@"searchBarSearchButtonClicked - %@", searchBar.text);
+    [self loadString:searchBar.text];
+}
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    NSLog(@"searchText - %@", searchText);
 }
 
 @end
