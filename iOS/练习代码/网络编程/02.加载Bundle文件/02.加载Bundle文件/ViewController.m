1041e4648358a55609b8ee47bd7b9fabbff467e0
railsgem <jun.chenying@hotmail.com>
2014-09-24 09:16:33 +0800

    Initial Commit

--- iOS/练习代码/网络编程/02.加载Bundle文件/02.加载Bundle文件/ViewController.m
+++ iOS/练习代码/网络编程/02.加载Bundle文件/02.加载Bundle文件/ViewController.m
@@ -10,20 +10,76 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIWebView *webView;
+
 @end
 
 @implementation ViewController
 
+
+-(UIWebView *)webView
+{
+    if (!_webView) {
+        _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+        _webView.dataDetectorTypes = UIDataDetectorTypeAll;
+    }
+    return _webView;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.webView];
+    
+    [self loadDataFile];
+//    [self.webView loadHTMLString:@"<h1>hello<h1/>" baseURL:nil];
+//    [self loadFile];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - 加载文件
+-(void)loadFile
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSURL *fileUrl = [[NSBundle mainBundle] URLForResource:@"iOS 7 Programming Cookbook.pdf" withExtension:nil];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:fileUrl];
+    
+    [self.webView loadRequest:request];
+    
+}
+
+
+#pragma mark - 以二进制的形式加载文件
+-(void)loadDataFile
+{
+    NSURL *fileUrl = [[NSBundle mainBundle] URLForResource:@"iOS 7 Programming Cookbook.pdf" withExtension:nil];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:fileUrl];
+    
+    //服务器响应
+    NSURLResponse *response = nil;
+    //发送请求给服务器
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:NULL];
+    
+    [self.webView loadData:data MIMEType:response.MIMEType textEncodingName:@"UTF8" baseURL:nil];
+    
+    NSLog(@"%@", response.MIMEType);
+    
+    
+}
+
+-(NSString *)mimeType:(NSURL *)url
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    //服务器响应
+    NSURLResponse *response = nil;
+    //发送请求给服务器
+    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:NULL];
+    
+    NSLog(@"%@", response.MIMEType);
+    
+    return response.MIMEType;
+    
 }
 
 @end
